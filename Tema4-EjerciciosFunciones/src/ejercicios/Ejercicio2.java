6d6c118fd7c9db4229dd49ed004de481058cdf56
jperez <jperez@106-20.info2>
2022-11-22 12:51:00 +0100

    ej 2 y 3 mirar que 3 funciona y comentar todos

---
+++ Tema4-EjerciciosFunciones/src/ejercicios/Ejercicio2.java
@@ -0,0 +1,39 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	
+	static void rango (int numero1, int numero2) {
+		while (numero1<=numero2) {
+		System.out.println(numero1);
+		numero1++;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		int numeroIntroducido;
+		int numeroIntroducido2;
+		
+		Scanner dogma = new Scanner(System.in);
+		
+				System.out.print("Introduzca el primer numero del rango: ");
+
+				numeroIntroducido = dogma.nextInt();
+				
+				System.out.print("Introduzca el segundo numero del rango: ");
+				
+				numeroIntroducido2 = dogma.nextInt();
+				
+				if (numeroIntroducido<numeroIntroducido2) {
+				rango (numeroIntroducido, numeroIntroducido2);
+				}
+				else {
+					rango (numeroIntroducido2, numeroIntroducido);
+				}
+				
+				dogma.close();
+	}
+
+}
---
+++ Tema4-EjerciciosFunciones/src/ejercicios/Ejercicio3.java
@@ -0,0 +1,48 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+	
+	static void cilindro (int seleccion, double radio, double altura) {
+		
+		switch (seleccion) {
+		
+		case 1 -> System.out.print("El area es: " + 2*Math.PI*radio);
+		
+		case 2 -> System.out.print("El volumen es: " + Math.PI*Math.pow(radio,2)*altura);
+		
+		default -> System.out.print("Error al seleccionar");
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+		double radio;
+		
+		double altura;
+		
+		int seleccion;
+		
+		Scanner dogma = new Scanner (System.in);
+		
+		System.out.print("Introduzca el radio de la base: ");
+		
+		radio = dogma.nextDouble();
+		
+		System.out.print("Introduzca la altura del cilindro: ");
+		
+		altura = dogma.nextDouble();
+		
+		System.out.print("Seleccione 1: para obtener el area del cilindro.\n 2: para obtener el volumen del cilindro");
+
+		seleccion = dogma.nextInt();
+		
+		cilindro (seleccion, radio, altura);
+		
+		dogma.close();
+		
+	}
+
+}
